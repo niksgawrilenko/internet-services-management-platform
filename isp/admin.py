18a5309005ba31369925a6f6e116b0812eea8f4b
niksgawrilenko <90038040+niksgawrilenko@users.noreply.github.com>
2023-09-01 10:29:38 +0300

    Add and editing the admin panel

--- isp/admin.py
+++ isp/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from isp.models import User, Address, TariffPlane, City
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("username", "phone",)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("country",)
+    list_display = ("name", "region", "country",)
+
+
+@admin.register(TariffPlane)
+class TariffPlaneAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("speed",)
+    list_display = ("name", "speed", "price",)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+
+    search_fields = ("city__name", "street",)
+    list_filter = ("city__name", "street", "connection_technology")
+    list_display = ("get_city_name", "street", "building", "connection_technology")
+
+    def get_city_name(self, obj) -> None:
+        return obj.city.name if obj.city else None
+
+    get_city_name.short_description = "City Name"
