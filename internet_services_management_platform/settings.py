0b3bf3ebcb53e4579935bc55e126098d0e67de94
niksgawrilenko <nikitagawrilenko2212@gmail.com>
2023-10-03 03:12:36 +0300

    Changes of presentation templates(addresses_list, customers_list,tariff_form, cities_list, customer_confirm_delete, index,customer_form,city_form, customer_detail, address_detail, city_confirm_delete, address_form, tariff_confirm_delete, address_confirm_delete, tariff_list) with Bootstrap usage.

--- internet_services_management_platform/settings.py
+++ internet_services_management_platform/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "isp.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -128,7 +129,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
+ASSETS_ROOT = "/static/assets"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
