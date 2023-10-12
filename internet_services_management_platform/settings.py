8dc24e265a56b936daf1902ae34ec8f617562b9b
niksgawrilenko <nikitagawrilenko2212@gmail.com>
2023-10-13 01:38:53 +0300

    Add cloud host

--- internet_services_management_platform/settings.py
+++ internet_services_management_platform/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "internet-services-management-platform-isp.onrender.com"]
 
 
 # Application definition
